42eda58ad1084b98142a9548a9baeb0be278487b
screpy <iosifscrepy@gmail.com>
2024-04-21 15:08:16 +0100

    Asd

--- src/pages/how-to-buy.jsx
+++ src/pages/how-to-buy.jsx
@@ -1,9 +1,11 @@
+import Cards from "../components/Cards/Cards";
 import Navbar from "../components/navbar/navbar";
 import "../global.css"
 const HowToBuy = () => {
     return (
         <div>
             <Navbar/>
+            <Cards/>
         </div>
     )
 }
--- src/pages/stacking.jsx
+++ src/pages/stacking.jsx
@@ -1,9 +1,11 @@
+import Cards from "../components/Cards/Cards";
 import Navbar from "../components/navbar/navbar";
 import "../global.css"
 const Stacking = () => {
     return (
         <div>
             <Navbar/>
+            <Cards/>
         </div>
     )
 }
